06ecf7740d1eec9c6a9d5f014ca023362bf1cf04
CDavantzis <c.davantzis@gmail.com>
2016-03-02 14:22:07 -0500

    Revert "Revert "Python Prototyping""
    This reverts commit a2346360bfb5331e2c0e7434f149848cab1cd919.

---
+++ prototyping/python_prototype_01.py
@@ -0,0 +1,32 @@
+''' Python Prototype To Outline How The C++ Code Will be Written '''
+
+# Math & Science Research and Sources
+# https://en.wikipedia.org/wiki/Gravitational_acceleration
+# https://en.wikipedia.org/wiki/Newton%27s_law_of_universal_gravitation
+# https://en.wikipedia.org/wiki/Gravitational_two-body_problem
+# https://en.wikipedia.org/wiki/Kepler%27s_laws_of_planetary_motion
+# https://en.wikipedia.org/wiki/N-body_problem#Planetary_problem
+# http://physics.tutorvista.com/motion/vector.html
+# http://mathinsight.org/vectors_cartesian_coordinates_2d_3d
+# http://web.mit.edu/pkrein/Public/Final%20Paper%20UW324.pdf
+# http://www.intmath.com/vectors/7-vectors-in-3d-space.php
+# http://www.physics.utah.edu/~sid/physics2010/Unit_Vectors.pdf
+
+# Programming Research & Sources
+# Better: https://github.com/bengolder/python-geometry/tree/master/geometry
+# Simple: http://hplgit.github.io/primer.html/doc/pub/class/._class-readable004.html#sec:class:Vec2D
+
+from math import sqrt, atan2, degrees
+
+G = 6.674e-11
+
+def Force(m1,m2,r):
+	return {"Fg":G*m1*m2/(r**2),
+			"a1":G*m2/(r**2),
+			"a2":G*m1/(r**2)}
+
+def Distance(x1,y1,x2,y2):
+	return {"r":sqrt((x2-x1)**2+(y2-y1)**2),
+			"theta": degrees(atan2(y2-y1,x2-x1))}
+	
+
