6d450043ced9385f7af984a094a2b1bbfbd39c14
CDavantzis <c.davantzis@gmail.com>
2016-03-02 14:25:17 -0500

    Fixed Spacing
    Changed Tabs To Spaces

--- prototyping/python_prototype_01.py
+++ prototyping/python_prototype_01.py
@@ -21,12 +21,12 @@ from math import sqrt, atan2, degrees
 G = 6.674e-11
 
 def Force(m1,m2,r):
-	return {"Fg":G*m1*m2/(r**2),
-			"a1":G*m2/(r**2),
-			"a2":G*m1/(r**2)}
+    return {"Fg":G*m1*m2/(r**2),
+            "a1":G*m2/(r**2),
+            "a2":G*m1/(r**2)}
 
 def Distance(x1,y1,x2,y2):
-	return {"r":sqrt((x2-x1)**2+(y2-y1)**2),
-			"theta": degrees(atan2(y2-y1,x2-x1))}
-	
+    return {"r":sqrt((x2-x1)**2+(y2-y1)**2),
+            "theta": degrees(atan2(y2-y1,x2-x1))}
+    
 
