f3c9e064b4f9d76280ab38010ebddf4e9e483442
CDavantzis <c.davantzis@gmail.com>
2016-03-02 16:40:09 -0500

    More Prototyping

--- prototyping/python_prototype_01.py
+++ prototyping/python_prototype_01.py
@@ -1,6 +1,7 @@
 ''' Python Prototype To Outline How The C++ Code Will be Written '''
 
 # Math & Science Research and Sources
+# http://scienceworld.wolfram.com/physics/topics/CelestialMechanics.html
 # https://en.wikipedia.org/wiki/Gravitational_acceleration
 # https://en.wikipedia.org/wiki/Newton%27s_law_of_universal_gravitation
 # https://en.wikipedia.org/wiki/Gravitational_two-body_problem
@@ -12,11 +13,15 @@
 # http://web.mit.edu/pkrein/Public/Final%20Paper%20UW324.pdf
 # http://www.intmath.com/vectors/7-vectors-in-3d-space.php
 # http://www.physics.utah.edu/~sid/physics2010/Unit_Vectors.pdf
-
+# http://astrowww.phys.uvic.ca/~tatum/celmechs.html
+# http://web.mit.edu/8.01t/www/materials/modules/chapter25.pdf
+# https://en.wikipedia.org/wiki/Orbital_mechanics
 # Programming Research & Sources
 # Better: https://github.com/bengolder/python-geometry/tree/master/geometry
 # Simple: http://hplgit.github.io/primer.html/doc/pub/class/._class-readable004.html#sec:class:Vec2D
-
+# http://astronomy.stackexchange.com/questions/7806/exercise-2d-orbital-mechanics-simulation-python
+# https://en.wikipedia.org/wiki/Celestial_mechanics
+# http://www.cs.cmu.edu/~scandal/alg/nbody.html
 from math import sqrt, atan2, degrees
 
 G = 6.674e-11
@@ -30,20 +35,7 @@ def Distance(x1,y1,x2,y2):
     return {"r":sqrt((x2-x1)**2+(y2-y1)**2),
             "theta": degrees(atan2(y2-y1,x2-x1))}
 
-class Point():
-	''' Point Class'''
-	pass
-
-class Vector():
-	''' Vector Class'''
-	pass
-
-class Body():
-	''' Body Object In N-body Simulation '''
-	pass
- 
 class Simulation():
-	''' Plane Class '''
 	def __init__(self,*bodies):
 		self.bodies = list(bodies)
 		self.time   = 0
@@ -54,10 +46,33 @@ class Simulation():
 	def add_body(self,body):
 		self.bodies.append(body)
 
-	def step_foward(self):
-		#make calculations
-		self.time += 1
 
+class Body():
+	''' Body Object In N-body Simulation '''
+	def __init__(self,mass,x,y,z,mu=None):
+		self.__mass = mass
+		if mu: #for greater accuaracy of known standard gravitational paramenter
+			self.__std_grav_param = mu
+		else:
+			self.__std_grav_param = G*mass
+
+	m  = property(lambda self: self.__mass)
+	mu = property(lambda self: self.__std_grav_param)
+
+
+class TwoBodySimulation():
+	#https://en.wikipedia.org/wiki/Gravitational_two-body_problem
+	#https://en.wikipedia.org/wiki/Two-body_problem
+	#https://en.wikipedia.org/wiki/Kepler_orbit
+	#https://en.wikipedia.org/wiki/Kepler_problem
+	#https://en.wikipedia.org/wiki/Equation_of_the_center
+	#http://www.astro.cornell.edu/~berthoud/alpsat/chapter4a.html
+	#https://en.wikipedia.org/wiki/Jacobi_coordinates
+	#http://scienceworld.wolfram.com/physics/Two-BodyProblem.html
+
+	def __init__(self,B1,B2):
+		self.bodies = [B1,B2]
+		self.time   = 0
 
 
 
