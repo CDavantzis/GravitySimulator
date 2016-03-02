a8390c01928d443de0c896511d2d4b45bf1886db
CDavantzis <c.davantzis@gmail.com>
2016-03-02 17:04:52 -0500

    Cleared

--- prototyping/python_prototype_01.py
+++ prototyping/python_prototype_01.py
@@ -12,36 +12,6 @@ def Distance(x1,y1,x2,y2):
     return {"r":sqrt((x2-x1)**2+(y2-y1)**2),
             "theta": degrees(atan2(y2-y1,x2-x1))}
 
-class Simulation():
-	def __init__(self,*bodies):
-		self.bodies = list(bodies)
-		self.time   = 0
-
-	def __len__(self):
-		return len(self.bodies)
-
-	def add_body(self,body):
-		self.bodies.append(body)
-
-
-class Body():
-	''' Body Object In N-body Simulation '''
-	def __init__(self,mass, rx, ry,):
-		self.__mass = mass
-
-	m  = property(lambda self: self.__mass)
-	mu = property(lambda self: G*self.mass)
-
-
-class TwoBodySimulation():
-
-
-	def __init__(self,B1,B2):
-		self.bodies = [B1,B2]
-		self.time   = 0
-
-
-
 
 
 
