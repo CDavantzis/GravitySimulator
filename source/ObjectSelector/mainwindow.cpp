863cd1f6f33135c85c3b3abea901d69043ee3ae6
CDavantzis <c.davantzis@gmail.com>
2016-04-03 02:35:43 -0400

    gui
    more updates to the gui

--- source/ObjectSelector/mainwindow.cpp
+++ source/ObjectSelector/mainwindow.cpp
@@ -1,14 +1,33 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent):QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
+
+    //bind buttons to fuctions
+    connect(ui->pushButton_add_row, SIGNAL(released()),this, SLOT(add_row()));
+    connect(ui->pushButton_remove_row, SIGNAL (released()),this, SLOT(remove_row()));
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow(){
     delete ui;
 }
+
+void MainWindow::add_row(){
+    //add row to object table
+    int current_row = ui->tableWidget->currentRow();
+    if (current_row == -1){
+        ui->tableWidget->insertRow(ui->tableWidget->rowCount());}
+    else{
+        ui->tableWidget->insertRow(current_row+1);}
+}
+
+void MainWindow::remove_row(){
+    //remove row from object table
+    int current_row = ui->tableWidget->currentRow();
+    if (current_row == -1){
+        ui->tableWidget->removeRow(ui->tableWidget->rowCount());}
+    else{
+        ui->tableWidget->removeRow(current_row);}
+}
+
--- source/ObjectSelector/mainwindow.h
+++ source/ObjectSelector/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTableWidget>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +15,17 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+private slots:
+    void add_row();
+    void remove_row();
 
 private:
     Ui::MainWindow *ui;
+
+    QTableWidget* m_pTableWidget;
+
+    QStringList m_TableHeader;
+
 };
 
 #endif // MAINWINDOW_H
