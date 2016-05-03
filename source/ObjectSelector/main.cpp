0eca2f1644ecdab7241a114777d9ea3541fd122e
CDavantzis <c.davantzis@gmail.com>
2016-05-03 04:25:06 -0400

    added seed for pseudo random function
    http://doc.qt.io/qt-4.8/qtglobal.html#qsrand

--- source/ObjectSelector/main.cpp
+++ source/ObjectSelector/main.cpp
@@ -1,8 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTime>
 
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
+    qsrand(QTime(0, 0, 0).secsTo(QTime::currentTime()));
     MainWindow w;
     w.show();
     return a.exec();
